ba5d77c45f513ee53161fc58e8864ea7cb4e54bb
Andres Pardini <unpardos@gmail.com>
2018-06-11 12:29:11 -0300

    add autocomplete for searchbox

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,8 +2,14 @@
   <b-row class="navheader p-2">
     <b-col md="5" align-self="center" class="p-1">
       <form v-on:submit.prevent="onSubmit">
-        <input class="form-control" v-model="search" type="search" placeholder="Search" aria-label="Search">
-        <i @click="clearSearch()" class="fa fa-2x fa-times"></i>
+        <input class="form-control" @focus="hasFocus = true" @blur="closeAutotomplete()" @input="searchInput" v-model="search" type="search" placeholder="Search" aria-label="Search">
+        <i v-show="search.length && !loading" @click="clearSearch()" class="fa fa-2x fa-times"></i>
+        <i v-show="loading" class="fa fa-2x fa-spinner fa-pulse"></i>
+        <div v-show="hasFocus && suggestions.length > 0" class="suggestions">
+          <div @click="useAutocomplete(suggestion)" v-for="suggestion in suggestions" class="suggestion" :key="suggestion">
+            {{suggestion}}
+          </div>
+        </div>
       </form>
     </b-col>
 
@@ -26,17 +32,41 @@
 </template>
 
 <script>
+import {debounce} from 'lodash/function'
 export default {
   name: 'Nav',
   data: function () {
     return {
+      'loading': false,
+      'hasFocus': false,
+      'suggestions': [],
       'search': '',
       playerApi: this.$root.playerApi
     }
   },
   methods: {
+    'closeAutotomplete': function () {
+      setTimeout(() => {
+        this.hasFocus = false
+      }, 200)
+    },
+    'searchInput': debounce(function () {
+      if (this.search.length <= 3) { return }
+      if (this.loading) { return }
+      this.loading = true
+      this.playerApi.getSuggestion(this.search).then(({data}) => {
+        this.suggestions = data
+      }).finally(() => {
+        this.loading = false
+      })
+    }, 500),
+    'useAutocomplete': function (search) {
+      this.search = search
+      this.playerApi.search(search)
+    },
     'clearSearch': function () {
       this.search = ''
+      this.suggestions = []
       this.playerApi.clearSearch()
     },
     'seek': function (event) {
--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="search-results" v-show="playerApi.searchResults.length">
-    <p>Resultados de la busqueda:</p>
-    <transition-group name="search-results" tag="div" class="list-unstyled">
-      <b-media tag="li" class="my-4" v-for="item in playerApi.searchResults" :key="item.id.videoId" v-if="!playerApi.isInQueue(item.id.videoId)">
-        <img slot="aside" :src="item.snippet.thumbnails.default.url" />
-        <p>{{item.snippet.title}}</p>
-        <b-button @click="playerApi.queueTrack(item.id.videoId)" :size="'sm'" :variant="'primary'">Queue</b-button>
-        <b-button @click="playerApi.queueTrackNext(item.id.videoId)" :size="'sm'" :variant="'primary'">Next</b-button>
-      </b-media>
-    </transition-group>
+  <div>
+    <div v-show="playerApi.searching" class="py-2">Loading...</div>
+    <div class="search-results" v-show="playerApi.searchResults.length">
+      <p>Resultados de la busqueda:</p>
+      <transition-group name="search-results" tag="div" class="list-unstyled">
+        <b-media tag="li" class="my-4" v-for="item in playerApi.searchResults" :key="item.id.videoId" v-if="!playerApi.isInQueue(item.id.videoId)">
+          <img slot="aside" :src="item.snippet.thumbnails.default.url" />
+          <p>{{item.snippet.title}}</p>
+          <b-button @click="playerApi.queueTrack(item.id.videoId)" :size="'sm'" :variant="'primary'">Queue</b-button>
+          <b-button @click="playerApi.queueTrackNext(item.id.videoId)" :size="'sm'" :variant="'primary'">Next</b-button>
+        </b-media>
+      </transition-group>
+    </div>
   </div>
 </template>
 
